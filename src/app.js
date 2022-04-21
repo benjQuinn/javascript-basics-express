fbbb9d434deb8b9fc611de57d94d6685103ec7ab
Ben Quinn <benjamesquinn@gmail.com>
2022-04-21 21:52:09 +0100

    Project set up and sayHello test passed

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const { sayHello } = require('./lib/strings')
 
 const app = express();
+
+app.get('/strings/hello/:string', (req, res) => {
+  const string  = req.params.string;
+  const result = sayHello(string);
+  
+  res.json({ result: `${result}` }).sendStatus(200);
+});
 
 module.exports = app;
