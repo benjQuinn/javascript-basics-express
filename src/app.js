a928c54e54d19daf57d50d75a4930791fff9bcc5
Ben Quinn <benjamesquinn@gmail.com>
2022-04-22 21:47:28 +0100

    Passed all strings tests

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const { sayHello } = require('./lib/strings')
+const { 
+  sayHello,  
+  uppercase, 
+  lowercase, 
+  firstCharacter, 
+  firstCharacters } = require('./lib/strings')
 
 const app = express();
 
@@ -9,5 +14,28 @@ app.get('/strings/hello/:string', (req, res) => {
   
   res.json({ result: `${result}` }).sendStatus(200);
 });
+
+app.get('/strings/upper/:string', (req, res) => {
+  const string = req.params.string;
+
+  res.json({ result: uppercase(string) }).sendStatus(200);
+})
+
+app.get('/strings/lower/:string', (req, res) => {
+  const string = req.params.string;
+
+  res.json({ result: lowercase(string) }).sendStatus(200);
+})
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  const string = req.params.string;
+
+  if (!req.query.length) {
+    res.json({ result: firstCharacter(string) }).sendStatus(200); 
+  } else {
+    res.json({ result: firstCharacters(string, req.query.length) }).sendStatus(200); 
+  }
+})
+
 
 module.exports = app;
