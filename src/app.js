c67561a7414b4d6ede504fc4634e9cf070740ae4
Ben Quinn <benjamesquinn@gmail.com>
2022-04-23 02:01:56 +0100

    Passed numbers addition test

--- src/app.js
+++ src/app.js
@@ -1,41 +1,59 @@
 const express = require('express');
+const { add } = require('./lib/numbers');
 const { 
   sayHello,  
   uppercase, 
   lowercase, 
   firstCharacter, 
-  firstCharacters } = require('./lib/strings')
+  firstCharacters, } = require('./lib/strings')
 
 const app = express();
 
+///////////STRINGS///////////
 app.get('/strings/hello/:string', (req, res) => {
   const string  = req.params.string;
   const result = sayHello(string);
   
-  res.json({ result: `${result}` }).sendStatus(200);
+  res.send(200).json({ result: `${result}` })
 });
 
 app.get('/strings/upper/:string', (req, res) => {
   const string = req.params.string;
 
-  res.json({ result: uppercase(string) }).sendStatus(200);
+  res.send(200).json({ result: uppercase(string) });
 })
 
 app.get('/strings/lower/:string', (req, res) => {
   const string = req.params.string;
 
-  res.json({ result: lowercase(string) }).sendStatus(200);
+  res.send(200).json({ result: lowercase(string) });
 })
 
 app.get('/strings/first-characters/:string', (req, res) => {
   const string = req.params.string;
 
   if (!req.query.length) {
-    res.json({ result: firstCharacter(string) }).sendStatus(200); 
+    res.send(200).json({ result: firstCharacter(string) });
   } else {
-    res.json({ result: firstCharacters(string, req.query.length) }).sendStatus(200); 
+    res.send(200).json({ result: firstCharacters(string, req.query.length) });
   }
-})
+
+});
+
+///////////NUMBERS///////////
+app.get('/numbers/add/:x/and/:y', (req, res) => {
+  const x = parseInt(req.params.x);
+  const y = parseInt(req.params.y);
+
+  if (x === 0 || y === 0) {
+    res.status(200).json({ result: add(x, y) });
+  } else if (!x || !y) {
+    res.status(400).json({ error: 'Parameters must be valid numbers.' })
+  } else {
+    res.status(200).json({ result: add(x, y)})
+  }
+  
+});
 
 
 module.exports = app;
